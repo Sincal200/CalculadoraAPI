eaca835a28d0c22bfd16f0a1efbe6ca36a2d9c41
Sincal <sincaljulio200@gmail.com>
2023-03-27 21:42:31 -0600

    Manejo de Error al dividir por cero

---
+++ demo/src/main/java/com/micompania/demo/DivisionByZeroException.java
@@ -0,0 +1,8 @@
+package com.micompania.demo;
+
+public class DivisionByZeroException extends RuntimeException {
+
+    public DivisionByZeroException(String message) {
+        super(message);
+    }
+}
